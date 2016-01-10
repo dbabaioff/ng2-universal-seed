6e3d9d2291228cb1aa2db904938695d88bc96ba2
dbabaioff <davidbabaioff@gmail.com>
2016-01-10 14:46:07 +0200

    Update deps

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>Hello Angular2</h1>'
+    //template: '<h1>Hello Angular2</h1>'
+    templateUrl: 'http://localhost:3000/src/app.html'
 })
 export class App {
     constructor() {
-        console.log('TEST');
+
     }
 }
